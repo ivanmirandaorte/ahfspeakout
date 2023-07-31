8b41a571b50a119a9d726eb90f7629e940f42d4b
ivanmirandaorte <ivan.miranda@ahf.org>
2023-07-31 09:43:04 -0700

    allowed theme to upload svg files

--- wp-content/themes/x/functions.php
+++ wp-content/themes/x/functions.php
@@ -63,3 +63,37 @@ function x_bootstrap() {
     [ class_exists( 'UberMenu' ), 'functions/plugins/ubermenu']
   ]
 ]);
+
+// Allow SVG Files 
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+  global $wp_version;
+  if ( $wp_version !== '6.2.2' ) {
+     return $data;
+  }
+
+  $filetype = wp_check_filetype( $filename, $mimes );
+
+  return [
+      'ext'             => $filetype['ext'],
+      'type'            => $filetype['type'],
+      'proper_filename' => $data['proper_filename']
+  ];
+
+}, 10, 4 );
+
+function cc_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+function fix_svg() {
+  echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+add_action( 'admin_head', 'fix_svg' );
